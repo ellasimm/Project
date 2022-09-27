813a30c0d03903e8397ea782129dbb1901aab8f2
ellasimm <ella.josephine.simm@gmail.com>
2022-09-27 18:48:59 -0400

    heyyyyyyyyyyyyy

---
+++ Project/src/Test1/Test2.java
@@ -0,0 +1,31 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a 4 digit number: ");
+		double num = input.nextDouble();
+		double num2 = num;
+		double rev = 0;
+		
+		for (int i = 0; i < 4; i++) {
+			double x = num % 10;
+			rev = rev + x;
+			num = Math.floorDiv((int) num, 10);
+		}
+		System.out.println("The sum of " + num2 + " is " + rev);
+					
+		
+		input.close();
+		
+		
+	}
+}
